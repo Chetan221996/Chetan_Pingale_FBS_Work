f43525f518b37f723856891c3dc64b5b10135b1a
Chetan Pingale <80944426+Chetan221996@users.noreply.github.com>
2025-11-08 11:40:58 +0530

    Stars Diamond

---
+++ Pattern Program/Stars.Daimond.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, space;
+    4
+    4printf("Enter the number of rows for the top half of the diamond: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for (i = n - 1; i >= 1; i--) {
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
