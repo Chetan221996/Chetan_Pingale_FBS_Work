339f664609668359f070a56f3513a283675cfb15
Chetan Pingale <80944426+Chetan221996@users.noreply.github.com>
2025-11-19 20:09:19 +0530

    Remove Odd Index Value

--- String/main.c
+++ String/main.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char str[100];
-    int len;
+    char str[200], result[200];
+    int i, j = 0;
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
-    str[strcspn(str, "\n")] = '\0';
+    for (i = 0; str[i] != '\0'; i++) {
+        if (i % 2 == 0) {
+            result[j++] = str[i];
 
-    len = strlen(str);
-
-    if (len > 1) {
-        char temp = str[0];
-        str[0] = str[len - 1];
-        str[len - 1] = temp;
+        }
     }
 
-    printf("New string after swapping first and last characters: %s\n", str);
+    result[j] = '\0';
+
+    printf("String after removing odd index characters: %s\n", result);
 
     return 0;
 }
