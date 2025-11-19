f92fca71fb85f05472e63968e272fa4cd893536c
Chetan Pingale <80944426+Chetan221996@users.noreply.github.com>
2025-11-19 20:22:47 +0530

    Palindrome String

--- String/main.c
+++ String/main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    char str[200], result[200];
-    int i, j = 0;
+    char str[200];
+    int i, len = 0, flag = 1;
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
-    for (i = 0; str[i] != '\0'; i++) {
-        if (i % 2 == 0) {
-            result[j++] = str[i];
+    while (str[len] != '\0') {
+        len++;
+    }
 
+    if (str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
+
+    for (i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            flag = 0;
+            break;
         }
     }
 
-    result[j] = '\0';
-
-    printf("String after removing odd index characters: %s\n", result);
+    if (flag)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is NOT a palindrome.\n");
 
     return 0;
 }
